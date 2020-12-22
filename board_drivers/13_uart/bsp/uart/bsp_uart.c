66f0b97ea3e4cad92a13ab87ceec95daa7519279
SunZhengh <1510214118@qq.com>
2020-12-22 01:03:23 -0800

    delete build

--- board_drivers/13_uart/bsp/uart/bsp_uart.c
+++ board_drivers/13_uart/bsp/uart/bsp_uart.c
@@ -5,6 +5,8 @@ void bsp_uart_init(void)
     CCM->CSCDR1 &= ~(1 << 6) ;
     CCM->CSCDR1 &= ~(0x3F << 0);
 
+    UART1->UCR1 &= ~(1 << 14);
+    UART1->UCR1 |= (1 << 0);
 }
 
 int bsp_uart_send(char* buf, int len)
